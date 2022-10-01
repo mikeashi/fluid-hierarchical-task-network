5bd6d7a6b9ef4a8aafdd1a736b9c5c50840731fa
Mike Ashi <mikeashi2013@gmail.com>
2022-10-01 19:03:59 +0200

    Update IDomain.cs

--- Fluid-HTN/IDomain.cs
+++ Fluid-HTN/IDomain.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using FluidHTN.Compounds;
 
 namespace FluidHTN
 {
-    public interface IDomain
+    public interface IDomain<TWorldStateEntry>
     {
-        TaskRoot Root { get; }
-        void Add(ICompoundTask parent, ITask subtask);
-        void Add(ICompoundTask parent, Slot slot);
+        TaskRoot<TWorldStateEntry> Root { get; }
+        void Add(ICompoundTask<TWorldStateEntry> parent, ITask<TWorldStateEntry> subtask);
+        void Add(ICompoundTask<TWorldStateEntry> parent, Slot<TWorldStateEntry> slot);
     }
 }
